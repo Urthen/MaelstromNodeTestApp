dca0fcd5eb3e94a96143deb68032eaaa7240da85
Michael Pratt <urthen@gmail.com>
2012-07-25 22:57:47 -0500

    ze api, it does nothing

--- app/controllers.js
+++ app/controllers.js
@@ -1,10 +1,13 @@
 module.exports = function(app){
 
+	var isProd = process.env.NODE_ENV == 'production',
+		apiHost = isProd ? 'http://prototype.projectmaelstrom.com' : 'http://prototype.projectmaelstrom.com:5000',
+		selfHost = isProd ? 'http://nodetest.projectmaelstrom.com' : 'http://nodetest.projectmaelstrom.com:4000';
+
 	function renderIndex(req, res) {
 		var user = "",
 			credentials = {};
-
-		res.render('index', {user: req.user});
+		res.render('index', {user: req.user, apiHost: apiHost, selfHost: selfHost});
 	};
 
 	app.get('/', renderIndex);
